9db5d0aced1dcad836de1044a433d12ceb0f356f
Dickie Stevens <rsk0528@gmail.com>
2018-10-22 19:25:47 -0400

    Rich Push

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,9 +1,10 @@
 import React from "react";
-import {Card, CardTitle} from 'react-materialize'
-import ChatRoom from "../../pages/ChatRoom/ChatRoom"
+import {Card, CardTitle} from 'react-materialize';
+import ChatRoom from "../../pages/ChatRoom/ChatRoom";
+import '../../pages/Chat/Chat.css';
 
 const Jumbotron = (props) => (
-  <Card style={{ width: '500px', height: '450px' }} header={<CardTitle reveal image={props.cardimage} waves='light'/>}
+  <Card style={{ width: '200px', height: '200px', borderRadius: '50%' }} header={<CardTitle reveal image={props.cardimage} waves='light'/>}
     reveal={<ChatRoom currenttopic={props.topic} user={props.userinfo} usethisurl={props.chaturl}/>}>
     {/* <p>Enter This Chat to talk about {children}!</p> */}
    {props.topic}
--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -4,21 +4,24 @@ import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row} from 'react-materialize'
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import Nav from "../../components/Nav"
+import Nav from "../../components/Nav";
 import io from "socket.io-client";
-import Login from "../Login/Login"
-import "./Chat.css"
-import ReactDOM from 'react-dom'
-import Flippy, { FrontSide, BackSide } from 'react-flippy';
-import ChatRoom from "../ChatRoom/ChatRoom"
-
+import Login from "../Login/Login";
+import "./Chat.css";
+import ReactDOM from 'react-dom';
+// import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import ChatRoom from "../ChatRoom/ChatRoom";
+import logo from "../../public/images/chatr-logo.png";
+import techLogo from '../../public/images/font-awesome/techChat.png';
+import musicLogo from '../../public/images/font-awesome/musicChat.png';
+import sportsLogo from '../../public/images/font-awesome/sportsChat.png';
+import moviesLogo from '../../public/images/font-awesome/moviesChat.png';
+import booksLogo from '../../public/images/font-awesome/booksChat.png';
 
 class Chat extends Component {
 
   constructor(props) {
     super(props);
-
-  
 
     this.state = {
       email: "",
@@ -27,28 +30,28 @@ class Chat extends Component {
       category: [
         {
           topic: "Tech",
-          img:'http://icons-for-free.com/free-icons/png/512/2890572.png',
+          img: techLogo,
           _id: 1, 
           url: 'localhost:3001'
         }, {
           topic: "Music",
-          img:'https://cdn1.iconfinder.com/data/icons/google_jfk_icons_by_carlosjj/512/music.png',
+          img: musicLogo,
           _id: 2, 
           url: 'localhost:3001/chat2'
         }, {
           topic: "Sports",
-          img:'https://www.shareicon.net/download/2016/08/18/814410_game_512x512.png',
+          img: sportsLogo,
           _id: 3, 
           url: 'localhost:3001/chat3'
         }, {
           topic: "Movies",
-          img:'https://www.shareicon.net/download/2016/07/05/791306_cinema_512x512.png',
+          img: moviesLogo,
           _id: 4, 
           url: 'localhost:3001/chat4'
         },
         {
           topic: "Books",
-          img:'https://i.imgur.com/kCSiMNB.png',
+          img: booksLogo,
           _id: 5, 
           url: 'localhost:3001/chat5'
         }]
@@ -87,61 +90,66 @@ class Chat extends Component {
 
   render() {
 
-
     const { isLoggedIn } = this.state
 
     if (isLoggedIn === false) {
       return (
         <div>
-      <div className="Login" >
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="email" bsSize="large">
-            <ControlLabel>Email</ControlLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            />
-          </FormGroup>
-          <Button
-            block
-            bsSize="large"
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Login
-          </Button>
-        </form>
-      </div>
+          <div className="Login" >
+            <form onSubmit={this.handleSubmit}>
+              <FormGroup controlId="email" bsSize="large">
+                <ControlLabel>Email</ControlLabel>
+                <FormControl
+                  autoFocus
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup controlId="password" bsSize="large">
+                <ControlLabel>Password</ControlLabel>
+                <FormControl
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  type="password"
+                />
+              </FormGroup>
+              <Button
+                block
+                bsSize="large"
+                disabled={!this.validateForm()}
+                type="submit"
+              >
+                Login
+              </Button>
+            </form>
+          </div>
+          <div className="footer"></div>
+          {/* Hidden Chat DIVS */}
+          <div id="chatBar" class="hidden">
+              <p>This is my chatBar div!</p>
+          </div>
       </div>
     );
     } else {
 
     return (
+      <div className="chatMain">
+      <img src={logo} alt="chatr" className="center"/>
       <div>
-      <img src="https://i.imgur.com/0HoUggK.png" alt="chatr" className="center"/>
-      <div>
-        <Row>
+        <Row style={{display: 'flex', justifyContent: 'center'}}>
  {this.state.category.map(topic => (
-          <Row>
-            <Col size="md-6">
-              <Jumbotron cardimage={topic.img} chaturl={topic.url} topic={topic.topic} userinfo={this.state.email}> 
-        
-              </Jumbotron>
-            </Col>
-          </Row>
+          <Col size="xs-12 md-6 lg-4 xl-4">
+            <Col size="md-1 lg-1 xl-1"></Col>
+            <Jumbotron cardimage={topic.img} chaturl={topic.url} topic={topic.topic} userinfo={this.state.email}> 
+      
+            </Jumbotron>
+            <Col size="md-1 lg-1 xl-1"></Col>
+          </Col>
         ))} 
         </Row>
         </div>
+        <div className="footerMain"></div>
         </div>
         )}}}
 
