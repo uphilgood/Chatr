9e8db44adfd32fd787de44598af5dfefc61bc68d
phil <philgoodmusic@gmail.com>
2018-10-23 16:31:05 -0400

    merged richs changes and fixed chatroom functionality

--- client/src/App.js
+++ client/src/App.js
@@ -19,12 +19,6 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Chat} />
         <Route exact path="/register" component={Register} />
-        {/* <Route exact path='/chat' component={Chat} /> */}
-        {/* <Route exact path="/chat/1" component={ChatRoom} />
-        <Route exact path="/chat/2" component={ChatRoom2} />
-        <Route exact path="/chat/3" component={ChatRoom3} />
-        <Route exact path="/chat/4" component={ChatRoom4} />
-        <Route exact path="/chat/5" component={ChatRoom5} /> */}
         <Route component={NoMatch} />
       </Switch>
     </div>
