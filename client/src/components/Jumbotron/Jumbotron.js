eef67a5ed88fd9c64f1ce71ff5e5c08e5da8d218
phil <philgoodmusic@gmail.com>
2018-10-23 14:22:04 -0400

    not changes made

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {Card, CardTitle} from 'react-materialize'
 import ChatRoom from "../../pages/ChatRoom/ChatRoom"
-
+// {{ width: '500px', height: '450px'}} 
 const Jumbotron = (props) => (
-  <Card style={{ width: '500px', height: '450px' }} header={<CardTitle reveal image={props.cardimage} waves='light'/>}
-    reveal={<ChatRoom currenttopic={props.topic} user={props.userinfo} usethisurl={props.chaturl}/>}>
+  <Card style={{ width: '500px', height: '450px'}} header={<CardTitle reveal image={props.cardimage} waves='light'/>}
+    reveal={<ChatRoom  currenttopic={props.topic} user={props.userinfo} usethisurl={props.chaturl} />}>
     {/* <p>Enter This Chat to talk about {children}!</p> */}
    {props.topic}
 </Card>
@@ -13,3 +13,4 @@ const Jumbotron = (props) => (
 export default Jumbotron;
 
 // {<CardTitle reveal image='https://i.imgur.com/0HoUggK.png' waves='light'/>}
+
