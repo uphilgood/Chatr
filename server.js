62d4fded502f8e483f7fde28853a9d80e603072e
phil <philgoodmusic@gmail.com>
2018-10-23 20:48:48 -0400

    removed line

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 // Start the API server
 const server = app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
